6ecd7b0522796bcac6ab41f760386d10c8dc3b3a
Vinni Hoke <vinni.hoke@gmail.com>
2020-05-18 13:26:48 -0700

    Added comments to recursive searching.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -34,50 +34,52 @@ def linear_search(arr, target):
     return -1
 
 # * Recursive binary search...
+# ! Causes infinite loop with the tests, but runs fine here. Returns value instead of index of value.
 # Write an iterative implementation of Binary Search
+
+# def binary_search(arr, target):
+#     if not arr:
+#         return -1
+#     # Your code here
+#     else:
+#         low = 0
+#         helper("arr[lowest]", low, 1)
+#         high = len(arr) - 1
+#         helper("arr[highest]", high, 1)
+#         mid = (high - low) // 2
+#         helper("Mid", mid, 1)
+
+#         if target == arr[mid]:
+#             helper("match value", arr[mid], 1)
+#             return mid
+#         elif target < arr[mid]:
+
+#             helper(f"{target} <", arr[mid], 1)
+#             helper("arr[:mid] new arr is", arr[:mid], 1)
+#             return binary_search(arr[:mid], target)
+#         else:
+#             helper(f"{target} >", arr[mid], 1)
+#             helper("arr[mid:] new arr is", arr[mid:], 1)
+#             return binary_search(arr[mid:], target)
+
+#     return -1  # not found
+
+# * Non-recursive binary search...
 
 
 def binary_search(arr, target):
-    if not arr:
-        return -1
-    # Your code here
-    else:
-        low = 0
-        helper("arr[lowest]", low, 1)
-        high = len(arr) - 1
-        helper("arr[highest]", high, 1)
-        mid = (high - low) // 2
-        helper("Mid", mid, 1)
-
-        if target == arr[mid]:
-            helper("match value", arr[mid], 1)
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]
+        if guess == target:
             return mid
-        elif target < arr[mid]:
-
-            helper(f"{target} <", arr[mid], 1)
-            helper("arr[:mid] new arr is", arr[:mid], 1)
-            return binary_search(arr[:mid], target)
+        if guess > target:
+            high = mid - 1
         else:
-            helper(f"{target} >", arr[mid], 1)
-            helper("arr[mid:] new arr is", arr[mid:], 1)
-            return binary_search(arr[mid:], target)
-
-    return -1  # not found
-
-# * Non-recursive binary search...
-# def binary_search(arr, target):
-#     low = 0
-#     high = len(arr) - 1
-#     while low <= high:
-#         mid = (low + high) // 2
-#         guess = arr[mid]
-#         if guess == target:
-#             return mid
-#         if guess > target:
-#             high = mid - 1
-#         else:
-#             low = mid + 1
-#     return -1
+            low = mid + 1
+    return -1
 
 
 # test = [-2, 7, 3, -9, 5, 1, 0, 4, -6]
